f27ee1d265803da4a5624a709fc935b66bacefdc
Arielle <40772024+ainabeari@users.noreply.github.com>
2021-01-16 15:57:23 -0500

    More main.py dev

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, redirect, url_for, render_template
 import json
+import numpy as np #for arrays
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index(): #Main dashboard (for one user)
+    #Reading User Data
+    with open('C:\\Users\\Arielle\\Documents\\GitHub\\HTN-2021\\data\\user_data.json', "r") as read_file:
+        data = json.load(read_file) #dictionary type
+
+    #Assigning variables for the user
+    name = data.get("name")
+    challenges = list(data.get("challenges").keys()) #This is 1D list of challenges. There are currently two challenges:
+    #challenge_1 = challenges[0] #This is the drinking water challenge
+    #Going to reformat this in arrays so it's easy to access
+    for key, value in data.items():
+        print(data.get(key))#list(data.get(key).items())
+
     return render_template("index.html")
 
 
