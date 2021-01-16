8e91a2801aa3698cbb0448dca6a6584b04f60c03
Allen Liu <58382354+allenl2@users.noreply.github.com>
2021-01-16 13:01:27 -0500

    Add challenges page

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/challenges")
+def challenges():
+    return render_template("challenges.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
