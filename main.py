56266e2e7165a371754fa987aafac807bfe0bdf5
Arielle <40772024+ainabeari@users.noreply.github.com>
2021-01-16 14:01:34 -0500

    Set up example JSON file

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask, redirect, url_for, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index(): #Main dashboard (for one user)
     return render_template("index.html")
 
 
 @app.route("/challenges")
-def challenges():
+def challenges(): #Page 3 (current challenges)
     return render_template("challenges.html")
+
+
 
 
 if __name__ == "__main__":
