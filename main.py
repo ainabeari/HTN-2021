987cb8b91c8326381449d66ff8e88e5a89926a83
Allen <2002allenliu@gmail.com>
2021-01-17 01:00:25 -0500

    more formatting + upload images

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def index():  # Main dashboard (for one user)
 
 @app.route("/challenges")
 def challenges():  # Page 3 (current challenges)
-    #def button():
-     #   print("Hello World")
+    # def button():
+    #   print("Hello World")
     return render_template("challenges.html")
 
 
@@ -36,28 +36,42 @@ def create_challenges():  # Page 3 (create new challenge)
         challenge_name = request.form['challenge_name']
         start = request.form['start']
         end = request.form['end']
-        task = request.form['task'] #This example only has one task
+        task = request.form['task']  # This example only has one task
         description = request.form['description']
         type = request.form['type']
-        #Insert math for calculating the progress
+        # Insert math for calculating the progress
         length = 0
 
-        #Updating json file (using example for user 2):
-        data.get('users').get('user2').get('challenges').update({challenge_name: {}})
-        data.get('users').get('user2').get('challenges').get(challenge_name).update({"Tasks": {}})
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").update({"Task 1": {}})
-        #Add other stuff like description, how often after check with Allen:
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").get("Task 1").update({"description": description})
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").get("Task 1").update({"type": type})
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").get("Task 1").update({"complete": False})
+        # Updating json file (using example for user 2):
+        data.get('users').get('user2').get(
+            'challenges').update({challenge_name: {}})
+        data.get('users').get('user2').get('challenges').get(
+            challenge_name).update({"Tasks": {}})
+        data.get('users').get('user2').get('challenges').get(
+            challenge_name).get("Tasks").update({"Task 1": {}})
+        # Add other stuff like description, how often after check with Allen:
+        data.get('users').get('user2').get('challenges').get(challenge_name).get(
+            "Tasks").get("Task 1").update({"description": description})
+        data.get('users').get('user2').get('challenges').get(
+            challenge_name).get("Tasks").get("Task 1").update({"type": type})
+        data.get('users').get('user2').get('challenges').get(challenge_name).get(
+            "Tasks").get("Task 1").update({"complete": False})
 
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").update({"Progress": {}})
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").get("Progress").update({"total days": length})
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").get("Progress").update({"current day": 0})
-        data.get('users').get('user2').get('challenges').get(challenge_name).get("Tasks").get("Progress").update({"progress": 0})
-
+        data.get('users').get('user2').get('challenges').get(
+            challenge_name).get("Tasks").update({"Progress": {}})
+        data.get('users').get('user2').get('challenges').get(challenge_name).get(
+            "Tasks").get("Progress").update({"total days": length})
+        data.get('users').get('user2').get('challenges').get(challenge_name).get(
+            "Tasks").get("Progress").update({"current day": 0})
+        data.get('users').get('user2').get('challenges').get(
+            challenge_name).get("Tasks").get("Progress").update({"progress": 0})
 
     return render_template("create-challenge.html", data=data)
+
+
+@app.route("/accountability")
+def accountability():  # Page 4 (new challenges - accountability)
+    return render_template("accountability.html")
 
 
 if __name__ == "__main__":
