6dd0e6cd55eef4d869631954628dfb6b97eb50f2
Allen <2002allenliu@gmail.com>
2021-01-17 05:10:30 -0500

    Final commit?

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from flask import Flask, redirect, url_for, render_template, request
 import json
-from google.cloud import firestore
+#from google.cloud import firestore
 
-#Initializing google cloud services
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+# Initializing google cloud services
+#import firebase_admin
+#from firebase_admin import credentials
+#from firebase_admin import firestore
 
 # Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-    'traq-18583': project_id,
-})
+#cred = credentials.ApplicationDefault()
+# firebase_admin.initialize_app(cred, {
+#    'traq-18583': project_id,
+# }
 
-db = firestore.client()
+#db = firestore.client()
 
-doc_ref = db.collection(u'users').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
-})
-
+#doc_ref = db.collection(u'users').document(u'alovelace')
+# doc_ref.set({
+#    u'first': u'Ada',
+#    u'last': u'Lovelace',
+#    u'born': 1815
+# })
 
 app = Flask(__name__)
 # Reading User Data
 ArielleDir = 'C:\\Users\\Arielle\\Documents\\GitHub\\HTN-2021\\data\\user_data.json'
 AllenDir = 'C:\\Users\\Allen\\Documents\\GitHub\\HTN-2021\\data\\user_data.json'
-with open(ArielleDir, "r") as read_file:
+with open(AllenDir, "r") as read_file:
     data = json.load(read_file)  # dictionary type
 
 # Assigning variables for the user
@@ -40,16 +39,14 @@ with open(ArielleDir, "r") as read_file:
 @app.route("/")
 def index():  # Main dashboard (for one user)
     # Some examples of getting some key data
-    name = data.get("users").get("user1").get("name")
-    challenges = list(data.get("users").get("user1").get("challenges"))
-    return render_template("index.html", content=name)
+    return render_template("index.html", data=data)
 
 
-@app.route("/challenges")
-def challenges():  # Page 3 (current challenges)
+@app.route("/challenges/<challenge_name>")
+def challenges(challenge_name):  # Page 3 (current challenges)
     # def button():
     #   print("Hello World")
-    return render_template("challenges.html")
+    return render_template("challenges.html", data=data)
 
 
 @app.route("/new-challenge", methods=("GET", "POST"))
@@ -57,6 +54,8 @@ def create_challenges():  # Page 3 (create new challenge)
     if request.method == 'POST':
         challenge_name = request.form['challenge_name']
         print(challenge_name)
+        return render_template("accountability.html", data=data)
+
         # start = request.form['start']
         # end = request.form['end']
         # task = request.form['task']  # This example only has one task
@@ -94,7 +93,22 @@ def create_challenges():  # Page 3 (create new challenge)
 
 @app.route("/accountability")
 def accountability():  # Page 4 (new challenges - accountability)
-    return render_template("accountability.html")
+    return render_template("accountability.html", data=data)
+
+
+@app.route("/add-people")
+def addPeople():  # Page 4 (new challenges - add people)
+    return render_template("add-people.html")
+
+
+@app.route("/home")
+def home():  # Page 4 (new challenges - add people)
+    return render_template("home.html")
+
+
+@app.route("/onboarding")
+def onboarding():  # Page 4 (new challenges - add people)
+    return render_template("onboarding.html")
 
 
 if __name__ == "__main__":
