12caa2ad9f8b826fbc0e4513cacc55bf6d39d4ac
Arielle <40772024+ainabeari@users.noreply.github.com>
2021-01-16 19:19:35 -0500

    Set up sample users in json file

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, redirect, url_for, render_template
+import json
 
 app = Flask(__name__)
+#Reading User Data
+ArielleDir = 'C:\\Users\\Arielle\\Documents\\GitHub\\HTN-2021\\data\\user_data.json'
+with open(ArielleDir, "r") as read_file:
+    data = json.load(read_file) #dictionary type
 
+#Assigning variables for the user
+#name = data.get("name") --> do this to get value of a key can use lists as well
+
+#for key, value in data.items():
+#    print(data.get(key))#list(data.get(key).items())
 
 @app.route("/")
 def index():  # Main dashboard (for one user)
+    #Some examples of getting some key data
+    name = data.get("users").get("user1").get("name")
+    challenges = list(data.get("users").get("user1").get("challenges"))
     return render_template("index.html")
 
 
